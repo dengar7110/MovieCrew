4159f7ce8b48ad3ba570853fd708ef7cc60305a0
garden <lmnoptnt@gmail.com>
2024-10-04 21:06:43 +0900

    [홍정원] - MovieCrew 프로젝트 최초생성

---
+++ src/main/java/com/garden/moviecrew/MovieCrewApplication.java
@@ -0,0 +1,13 @@
+package com.garden.moviecrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCrewApplication.class, args);
+	}
+
+}
